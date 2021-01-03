c2dd7e7e96b5dc1a448f29fede0011d66c28fecf
Ibrahim <53277964+ibrixxx@users.noreply.github.com>
2021-01-03 20:11:16 +0100

    Update super.js

--- routes/super.js
+++ routes/super.js
@@ -4,7 +4,7 @@ var pg = require("pg");
 var config = {
     user: 'kdypkdwr', //env var: PGUSER
     database: 'kdypkdwr', //env var: PGDATABASE
-    password: '-', //env var: PGPASSWORD
+    password: 'fRvdrdjOB4EvxSoSVkVTdA4EeBAAmvmX', //env var: PGPASSWORD
     host: 'kandula.db.elephantsql.com', // Server hosting the postgres database
     port: 5432, //env var: PGPORT
     max: 100, // max number of clients in the pool
@@ -12,6 +12,7 @@ var config = {
 };
 var pool = new pg.Pool(config);
 var pool2 = new pg.Pool(config);
+var pool3 = new pg.Pool(config);
 
 router.get('/', function(req, res, next) {
     let rest = {};
@@ -20,7 +21,7 @@ router.get('/', function(req, res, next) {
         if(err){
             res.end('{"error":"Error","status":500 }');
         }
-        client.query("SELECT naziv, kategorija, grad, email, ime_admina FROM restoran WHERE aktivan = true;", [], function (err, result){
+        client.query("SELECT id, naziv, kategorija, grad, email FROM restoran WHERE aktivan = true;", [], function (err, result){
             done();
             if(err){
                 console.log(err);
@@ -40,16 +41,124 @@ router.get('/', function(req, res, next) {
                         }
                         else{
                             g = result.rows;
-                            res.render('superAdmin', {data: rest, gr: g});
+                            pool3.connect(function (err, client3, done){
+                                if(err){
+                                    res.end('{"error":"Error","status":500 }');
+                                }
+                                client3.query("SELECT id, naziv FROM kategorija_hrane_lkp WHERE aktivan = true;", [], function (err, result){
+                                    done();
+                                    if(err){
+                                        console.log(err);
+                                        res.sendStatus(500);
+                                    }
+                                    else{
+                                        res.render('superAdmin', {data: rest, gr: g, hrana: result.rows});
+                                    }
+                                });
+                            });
                         }
                     });
                 });
             }
         });
     });
+});
 
+router.post('/delete/:id', function(req, res, next) {
+    pool.connect(function (err, client, done){
+        let id = req.params.id;
+        if(err){
+            res.end('{"error":"Error","status":500 }');
+        }
+        client.query(`UPDATE restoran SET aktivan = false WHERE id = $1;`, [id], function (err, result){
+            done();
+            if(err){
+                console.info(id);
+                res.sendStatus(500);
+            }
+            else{
+                console.log(id);
+            }
+        });
+    });
+});
 
+router.post('/delete/city/:id', function(req, res, next) {
+    pool.connect(function (err, client, done){
+        let id = req.params.id;
+        if(err){
+            res.end('{"error":"Error","status":500 }');
+        }
+        client.query(`UPDATE grad_lkp SET aktivan = false WHERE id = $1;`, [id], function (err, result){
+            done();
+            if(err){
+                console.info(id);
+                res.sendStatus(500);
+            }
+            else{
+                console.log(id);
+            }
+        });
+    });
+});
 
+router.post('/delete/food/:id', function(req, res, next) {
+    pool.connect(function (err, client, done){
+        let id = req.params.id;
+        if(err){
+            res.end('{"error":"Error","status":500 }');
+        }
+        client.query(`UPDATE kategorija_hrane_lkp SET aktivan = false WHERE id = $1;`, [id], function (err, result){
+            done();
+            if(err){
+                console.info(id);
+                res.sendStatus(500);
+            }
+            else{
+                console.log(id);
+            }
+        });
+    });
+});
+
+router.post('/addFood', function(req, res, next) {
+    pool.connect(function (err, client, done){
+        let fname = req.body.fname;
+        if(err){
+            res.end('{"error":"Error","status":500 }');
+        }
+        client.query(`INSERT INTO kategorija_hrane_lkp(naziv) VALUES ($1);`,
+            [fname], function (err, result){
+                done();
+                if(err){
+                    console.log(err);
+                    res.sendStatus(500);
+                }
+                else{
+                    console.log(fname);
+                }
+            });
+    });
+});
+
+router.post('/addCity', function(req, res, next) {
+    pool.connect(function (err, client, done){
+        let fname = req.body.CityName;
+        if(err){
+            res.end('{"error":"Error","status":500 }');
+        }
+        client.query(`INSERT INTO grad_lkp(naziv) VALUES ($1);`,
+            [fname], function (err, result){
+                done();
+                if(err){
+                    console.log(err);
+                    res.sendStatus(500);
+                }
+                else{
+                    console.log(fname);
+                }
+            });
+    });
 });
 
 module.exports = router;
